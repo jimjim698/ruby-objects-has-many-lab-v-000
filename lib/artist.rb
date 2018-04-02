bac7db25d10e7e8d7c81209757844c4f50a64add
James Lotruglio <jimlotruglio@yahoo.com>
2018-04-02 19:31:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
 attr_accessor :name
-@@all_songs = []
+@@song_count = 0
 
 def initialize(name)
   @name = name
@@ -14,18 +14,18 @@ end
 def add_song(song)
   @songs<< song
   song.artist = self
-  @@all_songs<<song
+  @@song_count += 1
 end
 
 def add_song_by_name(name)
   song = Song.new(name)
   @songs<< song
   song.artist = self
-  @@all_songs<<song
+  @@song_count += 1
 end
 
 def self.song_count
-  @@all_songs.length
+  @@song_count
 end
 
 end
